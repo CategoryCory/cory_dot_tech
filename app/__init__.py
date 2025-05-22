fb629aef063002576863db45e5400ff4786c176c
Cory Coward <ccoward80@gmail.com>
2025-05-21 21:57:46 -0400

    Resolve linting errors

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,6 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app.main import bp as main_bp
+from app.main import bp as main_bp  # noqa: E402
 app.register_blueprint(main_bp)
 
