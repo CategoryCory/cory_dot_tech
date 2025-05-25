cf1fc09b6e74eee4dbcbe0d38a5231f0eaa44b31
Cory Coward <ccoward80@gmail.com>
2025-05-24 23:28:51 -0400

    Add work experiences to index

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,14 @@
 from flask import render_template
+from sqlalchemy import desc, select
+from app.extensions import db
 from app.main import bp
+from app.models import WorkExperience
 
 @bp.route('/')
 def index():
-    return render_template('main/index.html')
+    get_work_exp_query = select(WorkExperience).order_by(desc(WorkExperience.start_date))
+    work_experiences = db.session.scalars(get_work_exp_query).all()
+    return render_template(
+        'main/index.html',
+        work_experiences=work_experiences
+    )
